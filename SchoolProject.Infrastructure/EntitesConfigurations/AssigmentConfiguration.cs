70e39ff165b36b06f23652f3ef7c7b5eb843deeb
Adham-hemida <127053745+Adham-hemida@users.noreply.github.com>
2025-07-12 23:28:33 +0300

    Added Assignment configuration

---
+++ SchoolProject.Infrastructure/EntitesConfigurations/AssigmentConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SchoolProject.Domain.Entites;
+
+namespace SchoolProject.Infrastructure.EntitesConfigurations;
+public class AssigmentConfiguration : IEntityTypeConfiguration<Assignment>
+{
+	public void Configure(EntityTypeBuilder<Assignment> builder)
+	{
+		builder.Property(x => x.Title).HasMaxLength(100);
+	}
+}
