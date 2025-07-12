ce7e561231de5144ecaa80d24813e9483706c5fb
Adham-hemida <127053745+Adham-hemida@users.noreply.github.com>
2025-07-12 23:23:57 +0300

    Added Department Configuration

--- SchoolProject.Domain/Entites/Teacher.cs
+++ SchoolProject.Domain/Entites/Teacher.cs
@@ -4,7 +4,7 @@ public class Teacher
 	public Guid Id { get; set; } = Guid.CreateVersion7();
 	public string FirstName { get; set; } = default!;
 	public string LastName { get; set; } = default!;
-	public string Email { get; set; } = default!;
+	public string? Email { get; set; } 
 	public string? Phone { get; set; }
 
 	public ICollection<Subject> Subjects { get; set; } =[];
---
+++ SchoolProject.Infrastructure/EntitesConfigurations/DepartmentConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SchoolProject.Domain.Entites;
+
+namespace SchoolProject.Infrastructure.EntitesConfigurations;
+public class DepartmentConfiguration : IEntityTypeConfiguration<Department>
+{
+	public void Configure(EntityTypeBuilder<Department> builder)
+	{
+		builder.Property(d => d.Name).IsRequired().HasMaxLength(100);
+		builder.Property(d => d.Description).HasMaxLength(500);
+	}
+}
+
