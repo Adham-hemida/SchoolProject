450b69aa5bc3f8b3afa8190f779cd4cabd2e4d4a
Adham-hemida <127053745+Adham-hemida@users.noreply.github.com>
2025-07-12 18:27:20 +0300

    Add project files.

---
+++ SchoolProject.Domain/Entites/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace SchoolProject.Domain.Entites;
+
+public class Student
+{
+	public int Id { get; set; }
+	public string Name { get; set; } = default!;
+	public string Address { get; set; } = default!;
+	public string Phone { get; set; } = default!;
+	public bool IsActive { get; set; } = true;
+}
