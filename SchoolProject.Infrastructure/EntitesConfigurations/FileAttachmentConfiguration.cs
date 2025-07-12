c991a4b272ff749254648d0d0b8be83c032c57aa
Adham-hemida <127053745+Adham-hemida@users.noreply.github.com>
2025-07-12 23:26:21 +0300

    Added FileAttachment Condiguration

---
+++ SchoolProject.Infrastructure/EntitesConfigurations/FileAttachmentConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SchoolProject.Domain.Entites;
+
+namespace SchoolProject.Infrastructure.EntitesConfigurations;
+public class FileAttachmentConfiguration:IEntityTypeConfiguration<FileAttachment>
+{
+	public void Configure(EntityTypeBuilder<FileAttachment> builder)
+	{
+		builder.Property(x => x.FileName).HasMaxLength(250);
+		builder.Property(x => x.StoredFileName).HasMaxLength(250);
+		builder.Property(x => x.ContentType).HasMaxLength(50);
+		builder.Property(x => x.FileExtension).HasMaxLength(10);
+
+	}
+}
