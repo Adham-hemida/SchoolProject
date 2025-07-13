e3f7582a363b668e06786370c94c42384c9e91dd
Adham-hemida <127053745+Adham-hemida@users.noreply.github.com>
2025-07-13 14:33:36 +0300

    added Unique Index To DepartmentSubject

---
+++ SchoolProject.Infrastructure/migrations/20250713113053_AddUniqueIndexToDepartmentSubject.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SchoolProject.Infrastructure.migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqueIndexToDepartmentSubject : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_DepartmentSubjects_DepartmentId",
+                table: "DepartmentSubjects");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DepartmentSubjects_DepartmentId_SubjectId",
+                table: "DepartmentSubjects",
+                columns: new[] { "DepartmentId", "SubjectId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_DepartmentSubjects_DepartmentId_SubjectId",
+                table: "DepartmentSubjects");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DepartmentSubjects_DepartmentId",
+                table: "DepartmentSubjects",
+                column: "DepartmentId");
+        }
+    }
+}
