e0d159de21c270ff9bb6464a0c079566bba352eb
Adham-hemida <127053745+Adham-hemida@users.noreply.github.com>
2025-07-14 17:56:52 +0300

    Added Get Student EndPoint

---
+++ SchoolProject.Api/Controllers/StudentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SchoolProject.Application.Abstractions;
+using SchoolProject.Application.Interfaces.IServices;
+namespace SchoolProject.Api.Controllers;
+[Route("api/Department/{DepartmentId}[controller]")]
+[ApiController]
+public class StudentsController(IStudentService studentService) : ControllerBase
+{
+	private readonly IStudentService _studentService = studentService;
+
+	[HttpGet("{id}")]
+	public async Task<IActionResult> GetById([FromRoute] int DepartmentId, [FromRoute] Guid id, CancellationToken cancellationToken = default)
+	{
+		var result = await _studentService.GetAsync(DepartmentId, id, cancellationToken);
+		return result.IsSuccess ? Ok(result.Value) : result.ToProblem();
+	}
+}
---
+++ SchoolProject.Application/Contracts/Student/StudentRequestValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+
+namespace SchoolProject.Application.Contracts.Student;
+public class StudentRequestValidator : AbstractValidator<StudentRequest>
+{
+	public StudentRequestValidator()
+	{
+		RuleFor(x=>x.FirstName)
+			.NotEmpty().WithMessage("First name is required.")
+			.Length(3,50)
+			.WithMessage("Max is 50 characters.");
+		
+		
+		RuleFor(x=>x.LastName)
+			.NotEmpty().WithMessage("First name is required.")
+			.Length(3,50)
+			.WithMessage("Max 50 characters.");
+
+		RuleFor(x=>x.Phone)
+			.NotEmpty()
+			.WithMessage("Phone number is required.")
+			.MaximumLength(15)
+			.WithMessage("Phone number must not exceed 15 characters.");
+
+		RuleFor(x=>x.Address)
+			.NotEmpty()
+			.MaximumLength(100)
+			.WithMessage("Address must not exceed 100 characters.");
+	}
+}
