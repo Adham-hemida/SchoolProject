5fd6410fc4bc106243a38a9942111c8f8b727d53
Adham-hemida <127053745+Adham-hemida@users.noreply.github.com>
2025-07-15 22:35:47 +0300

    Added Validation on Department request

---
+++ SchoolProject.Application/Contracts/Department/DepartmentRequestValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolProject.Application.Contracts.Department;
+public class DepartmentRequestValidator:AbstractValidator<DepartmentRequest>
+{
+	public DepartmentRequestValidator()
+	{
+		RuleFor(x => x.Name)
+			.NotEmpty()
+			.MaximumLength(100).WithMessage("Department name must not exceed 100 characters.");
+	}
+}
