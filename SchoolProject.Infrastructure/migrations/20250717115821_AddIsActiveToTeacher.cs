d0206bf67c8af34a95d0058f35de2c4e838b8833
Adham-hemida <127053745+Adham-hemida@users.noreply.github.com>
2025-07-17 15:07:20 +0300

    Added IsActive in Teacher Table

---
+++ SchoolProject.Infrastructure/migrations/20250717115821_AddIsActiveToTeacher.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SchoolProject.Infrastructure.migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsActiveToTeacher : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Teachers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Teachers");
+        }
+    }
+}
