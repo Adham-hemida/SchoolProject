59c68fe15c8e2cdd3de6c7daefe3cce6caaf9686
Adham-hemida <127053745+Adham-hemida@users.noreply.github.com>
2025-07-24 20:37:50 +0300

    Added  Profile Info Endpoint

---
+++ SchoolProject.Api/Controllers/AccountsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SchoolProject.Application.Extensions;
+using SchoolProject.Application.Interfaces.IAuthentication;
+
+namespace SchoolProject.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class AccountsController(IAccountService accountService) : ControllerBase
+{
+	private readonly IAccountService _accountService = accountService;
+
+	[HttpGet("")]
+	public async Task<IActionResult> ProfileInfo(CancellationToken cancellationToken)
+	{
+		var result = await _accountService.GetProfileInfoAsync(User.GetUserId()!, cancellationToken);
+		return Ok(result.Value);
+	}
+}
---
+++ SchoolProject.Infrastructure/Implementation/Authentication/AccountService.cs
@@ -0,0 +1,26 @@
+﻿using Mapster;
+using Microsoft.AspNetCore.Identity;
+using SchoolProject.Application.Abstractions;
+using SchoolProject.Application.Contracts.Authentication;
+using SchoolProject.Application.Interfaces.IAuthentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolProject.Infrastructure.Implementation.Authentication;
+public class AccountService(UserManager<ApplicationUser> userManager) : IAccountService
+{
+	private readonly UserManager<ApplicationUser> _userManager = userManager;
+
+	public async Task<Result<UserProfileResponse>> GetProfileInfoAsync(string userId,CancellationToken cancellationToken=default)
+	{
+		var user = await _userManager.Users
+		   .Where(x => x.Id == userId)
+		   .ProjectToType<UserProfileResponse>()
+		   .SingleAsync(cancellationToken);
+
+		return Result.Success(user);
+	}
+}
