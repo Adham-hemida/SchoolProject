fb72cfb84bddeef74d0ca20ce35d24f8a97f48ca
Adham-hemida <127053745+Adham-hemida@users.noreply.github.com>
2025-07-25 21:30:17 +0300

    Added Email Service

---
+++ SchoolProject.Application/Settings/MailSettings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolProject.Application.Settings;
+public class MailSettings
+{
+	[Required, EmailAddress]
+	public string Mail { get; set; } = string.Empty;
+	[Required]
+	public string DisplayName { get; set; } = string.Empty;
+	[Required]
+	public string Password { get; set; } = string.Empty;
+	[Required]
+	public string Host { get; set; } = string.Empty;
+	[Range(100, 999)]
+	public int Port { get; set; }
+}
+
